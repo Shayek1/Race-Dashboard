f733751dab99bd88d02979a171010bb423c99fca
Shayek Ahmed <shayek.a@outlook.com>
2025-10-25 01:57:09 +0100

    fix syntax errors

--- src/data.jsx
+++ src/data.jsx
@@ -1,7 +1,7 @@
-export const racingData = {
+export const racingData = [
    {lap: 1, Verstappen: 60.5, Hamilton: 65.0, Norris: 70.0},
    {lap: 2, Verstappen: 55.2, Hamilton: 50.0, Norris: 56.8},
    {lap: 3, Verstappen: 70.1, Hamilton: 65.0, Norris: 70.8},
    {lap: 4, Verstappen: 70.5, Hamilton: 68.3, Norris: 74.2},
    {lap: 5, Verstappen: 64.0, Hamilton: 55.9, Norris: 77.7},
-}
+];
