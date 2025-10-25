27352ee6276785c858682736a4d5940872b54927
Shayek Ahmed <shayek.a@outlook.com>
2025-10-25 13:21:51 +0100

    Change App.jsx to RaceMetrics.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import RaceMetrics from './App.jsx'
+import RaceMetrics from './RaceMetrics.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
