f6bc4b1a5911715d2442b74f8a9dd72926e99dad
Shayek Ahmed <shayek.a@outlook.com>
2025-10-25 02:10:22 +0100

    create axis and titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {racingData} from "./data.jsx";
+import{ Line, XAxis, YAxis, Legend, LineChart, Tooltip, ResponsiveContainer} from "recharts";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+<div>
+    <h1>F1 Race Metrics</h1>
+<div>
+  <h2>
+    Lap Time Comparison
+  </h2>
+  <ResponsiveContainer width="100%" height={300}>
+    <LineChart data={racingData}>
+      <XAxis dataKey="lap" label={{value: "lap", position: "insideBottom", offset: -5}} />
+      <YAxis label={{value: "Lap Time(s)", angle: -90, position:"insideLeft"}} />
+    </LineChart>
+  </ResponsiveContainer>
+</div>
+</div>
+  );
 }
 
 export default App
