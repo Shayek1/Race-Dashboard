b2d7dfb01cc2df62e88726afee239dd7d6239df6
Shayek Ahmed <shayek.a@outlook.com>
2025-10-25 13:18:57 +0100

    update Tailwind classNames for improved UI design

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import{ Line, XAxis, YAxis, Legend, LineChart, Tooltip, ResponsiveContainer} fro
 function App() {
 
   return (
-<div>
-    <h1>F1 Race Metrics</h1>
-<div>
-  <h2>
+<div className="p-8 bg-gray-100 min-h-screen rounded-2xl">
+    <h1 className="text-3xl text-black-600 font-bold mb-6 text-center">F1 Race Metrics</h1>
+<div className="bg-white rounded-2xl p-6 shadow-md">
+  <h2 className="text-xl font-semibold mb-4">
     Lap Time Comparison
   </h2>
   <ResponsiveContainer width="100%" height={300}>
@@ -16,7 +16,7 @@ function App() {
       <XAxis dataKey="lap" label={{value: "lap", position: "insideBottom", offset: -5}} />
       <YAxis label={{value: "Lap Time(s)", angle: -90, position:"insideLeft"}} />
       <Tooltip/>
-      <Legend/>
+      <Legend align="center" iconSize={10} />
       <Line type="monotone" dataKey="Verstappen" stroke="#3DB7E4"/>
       <Line type="monotone" dataKey="Hamilton" stroke="#FF8849"/>
       <Line type="monotone" dataKey="Norris" stroke="#69BE2B"/>
