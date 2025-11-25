e866a525d12599553b1f2b727e432938f2d57b6b
Shayek Ahmed <shayek.a@outlook.com>
2025-11-25 01:28:08 +0000

    adding Redux globally

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import RaceMetrics from './RaceMetrics.jsx'
+import { Provider } from "react-redux";
+import { store} from "./store/storeTheme.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RaceMetrics />
+   <Provider store={store}>
+       <RaceMetrics />
+   </Provider>
   </StrictMode>,
 )
